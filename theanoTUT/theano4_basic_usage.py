77979dbb1db8b4894bde9259ced17bf19bf0532d
wangweiboss <1017395252@qq.com>
2016-08-08 23:36:44 +0800

    Update theano4_basic_usage.py

--- theanoTUT/theano4_basic_usage.py
+++ theanoTUT/theano4_basic_usage.py
@@ -17,7 +17,7 @@ y = T.dscalar('y')
 z = x+y     # define the actual function in here
 f = function([x, y], z)  # the inputs are in [], and the output in the "z"
 
-print(f(2,3))  # only give the inputs "x and y" for this function, then it will calculate the output "z"
+print(f(2,3))  # only give the inputs "x and y" for this function, then it will calculate the output "z"注意传参的方式，没有[]
 
 # to pretty-print the function
 from theano import pp
@@ -28,4 +28,4 @@ x = T.dmatrix('x')
 y = T.dmatrix('y')
 z = x + y
 f = function([x, y], z)
-print(f(np.arange(12).reshape((3,4)), 10*np.ones((3,4))))
+print(f(np.arange(12).reshape((3,4)), 10*np.ones((3,4))))#注意ones的使用方法ones(shape, dtype, order)
